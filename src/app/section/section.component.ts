7fe2dd7a6ae8a6385baf31b8e6d4394213492658
BomLee427 <bomlee427@gmail.com>
2024-07-29 18:10:22 +0900

    컴포넌트 재사용 예제

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -7,18 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionComponent implements OnInit {
 
-  // present: string = 'welcome';
+  present: string = 'welcome';
+
+  // 프로퍼티 초기화 안할 시 typescript 오류 발생
+  // https://velog.io/@gingaminga/%EB%AC%B8%EC%A0%9C-%ED%95%B4%EA%B2%B0-has-no-initializer-and-is-not-definitely-assigned-in-the-constructor
+  commandText: string;
 
   // 문제: 버튼을 눌렀을 때 다른 컴포넌트에 데이터가 전달이 되어야 한다.
   // 설계 문제. 정답은 없다.
 
 
   // 변수명은 상관 없지만 헷갈리지 말라고 통상적으로 $event로 적음
-  // startTime($event: string) {
-  //   this.present = $event;
-  // }
+  startTime($event: string) {
+    this.present = $event;
+  }
 
-  constructor() { }
+  constructor() { 
+    this.commandText = '';
+  }
 
   ngOnInit(): void {
   }
